a23d64c340bc66480650bbde420f49b630eb5cd0
pepe217 <michael.r.pepe@gmail.com>
2024-06-23 10:09:13 -0700

    Don't highlight during char jumps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -266,7 +266,9 @@ require('lazy').setup({
     'folke/flash.nvim',
     event = 'VeryLazy',
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      modes = { char = { highlight = { backdrop = false } } },
+    },
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
