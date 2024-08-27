8e075137f4ff86fe491994c56a01975cf3dc1b8b
pepe217 <michael.r.pepe@gmail.com>
2024-08-26 19:37:34 -0700

    Proper capitalization

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -918,8 +918,8 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['tab'] = cmp.mapping.confirm(),
-          ['<S-tab>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace },
+          ['Tab'] = cmp.mapping.confirm(),
+          ['<S-Tab>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
