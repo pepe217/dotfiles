e3b605148e1e4e01a5872414fa57b8ed5028df8e
pepe217 <michael.r.pepe@gmail.com>
2024-08-26 19:37:51 -0700

    Insert a line properly.

--- nvim/.config/nvim/lua/test.lua
+++ nvim/.config/nvim/lua/test.lua
@@ -46,6 +46,7 @@ function M.add_to_decorator(input)
     M.add_full_decorator(buf, function_node, input)
   else
     -- some decorators exist
+    -- this checks for the measurement already existing
     local query = vim.treesitter.query.parse(
       'python',
       [[
@@ -59,13 +60,18 @@ function M.add_to_decorator(input)
     for _, match, _ in query:iter_matches(decorator_node, buf) do
       local row, column, _ = match[2]:end_()
       local last_char = vim.api.nvim_buf_get_text(buf, row, column - 2, row, column - 1, {})[1]
-      local new_meas = string.format('asd.measure(%s),', input)
+      local new_meas = string.format("asd.measure('%s'),", input)
       if last_char == ')' then
         new_meas = ', ' .. new_meas
       end
       vim.api.nvim_buf_set_text(buf, row, column - 1, row, column - 1, { new_meas })
     end
   end
+  -- get indentation level for inserting new text
+  local non_blank = vim.api.nvim_get_current_line():match('^%s*'):len()
+  vim.api.nvim_buf_set_lines(buf, pos[1], pos[1], true, { string.rep(' ', non_blank) .. 'asdfasdf = ' })
+  vim.api.nvim_win_set_cursor(0, { pos[1] + 1, 0 })
+  vim.cmd 'startinsert!'
 end
 
 function M.add_full_decorator(buf, function_node, name)
