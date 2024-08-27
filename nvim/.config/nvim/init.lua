bc02a48609530e2f9479f3f58d9a0cf5f02ddfde
pepe217 <michael.r.pepe@gmail.com>
2024-08-26 19:52:48 -0700

    Typo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -918,7 +918,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['Tab'] = cmp.mapping.confirm(),
+          ['<Tab>'] = cmp.mapping.confirm(),
           ['<S-Tab>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace },
 
           -- If you prefer more traditional completion keymaps,
