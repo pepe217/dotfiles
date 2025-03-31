b75388ccd1083d84719b26f31b3f62861d83fdcd
pepe217 <michael.r.pepe@gmail.com>
2025-03-31 09:18:24 -0700

    Remove linting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1194,30 +1194,6 @@ require('lazy').setup({
     end,
   },
   {
-    { -- Linting
-      'mfussenegger/nvim-lint',
-      event = { 'BufReadPre', 'BufNewFile' },
-      config = function()
-        local lint = require 'lint'
-        lint.linters_by_ft = {
-          markdown = { 'markdownlint' },
-          python = { 'flake8' },
-          json = { 'jsonlint' },
-        }
-
-        -- Create autocommand which carries out the actual linting
-        -- on the specified events.
-        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-        vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-          group = lint_augroup,
-          callback = function()
-            require('lint').try_lint()
-          end,
-        })
-      end,
-    },
-  },
-  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     -- Optional dependency
