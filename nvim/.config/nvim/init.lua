7448e0963557af2be704b7da678df895a9cb9ba6
pepe217 <michael.r.pepe@gmail.com>
2025-03-31 09:20:32 -0700

    Add context header

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1169,6 +1169,37 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {
+          -- Avoid the sticky context from growing a lot.
+          max_lines = 3,
+          -- Match the context lines to the source code.
+          multiline_threshold = 1,
+          -- Disable it when the window is too small.
+          min_window_height = 20,
+        },
+        keys = {
+          {
+            '[c',
+            function()
+              -- Jump to previous change when in diffview.
+              if vim.wo.diff then
+                return '[c'
+              else
+                vim.schedule(function()
+                  require('treesitter-context').go_to_context()
+                end)
+                return '<Ignore>'
+              end
+            end,
+            desc = 'Jump to upper context',
+            expr = true,
+          },
+        },
+      },
+    },
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'python', 'proto', 'yaml' },
