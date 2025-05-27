b663d16874febeeb6cb3326c50dc4e10ae8a5153
pepe217 <michael.r.pepe@gmail.com>
2025-05-26 21:27:32 -0700

    Use more builtin keymaps.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -575,7 +575,7 @@ require('lazy').setup({
         desc = 'Goto Definition',
       },
       {
-        'gr',
+        '<leader>r',
         function()
           Snacks.picker.lsp_references()
         end,
@@ -901,18 +901,6 @@ require('lazy').setup({
             end, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Rename the variable under your cursor.
-          map('<leader>r', vim.lsp.buf.rename, '[R]e[n]ame')
-
-          -- Opens a popup that displays documentation about the word under your cursor
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.server_capabilities.documentHighlightProvider then
